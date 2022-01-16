a4b77ebd7d2fdfb315c781b6f228d32e09358c0e
leofk <73722423+leofk@users.noreply.github.com>
2022-01-16 14:48:18 +0200

    Color the Sphere with Fragment Normals.

--- glsl/sphere.fs.glsl
+++ glsl/sphere.fs.glsl
@@ -1,6 +1,11 @@
+varying vec3 vNormal;
+
 void main() {
 
  	// HINT: Q1b, Set final rendered color surface normals
-  	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // REPLACE ME
+
+	//vec3 adjNormal = normalize(vNormal) * 0.5 + 0.5; // normalize normal
+
+  	gl_FragColor = vec4(vNormal, 1.0); // REPLACE ME
 
 }
